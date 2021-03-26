2bc1f42849a965a4532ae244d6a8efe91a09f9eb
Anas mansuri <ansamansuri633@gmail.com>
2021-03-26 15:58:32 +0530

    Added some pages

--- eg/shop/urls.py
+++ eg/shop/urls.py
@@ -4,5 +4,16 @@ from .import views
 
 urlpatterns = [
     path('',views.index,name="ShopHome"),
-    path('contact/',views.contact,name="ShopContact")
+    path('contact/',views.contact,name="ShopContact"),
+    path('account/',views.account,name="ShopAccount"),
+    path('eyeglass/',views.eyeglass,name="ShopEyeGlass"),
+    path('goggle/',views.goggle,name="ShopGoggle"),
+    path('productframe/',views.productframe,name="ShopProductFrame"),
+    path('productframe1/',views.productframe1,name="ShopProductFrame1"),
+    path('productframe2/',views.productframe2,name="ShopProductFrame2"),
+    path('productframe3/',views.productframe3,name="ShopProductFrame3"),
+    path('productframe4/',views.productframe4,name="ShopProductFrame4"),
+    path('cart/',views.cart,name="ShopCart"),
+    path('checkout/',views.checkout,name="ShopCheckout"),
+    path('shoplogin/',views.shoplogin,name="ShopLogin"),
 ]
--- eg/shop/views.py
+++ eg/shop/views.py
@@ -7,3 +7,36 @@ def index(request):
 
 def contact(request):
     return render(request,'shop/contact.html')
+
+def account(request):
+    return render(request,'shop/account.html')
+
+def eyeglass(request):
+    return render(request,'shop/eyeglass.html')
+
+def goggle(request):
+    return render(request,'shop/goggle.html')
+
+def cart(request):
+    return render(request,'shop/cart.html')
+
+def checkout(request):
+    return render(request,'shop/checkout.html')
+
+def productframe(request):
+    return render(request,'shop/productframe.html')
+
+def productframe1(request):
+    return render(request,'shop/productframe1.html')
+
+def productframe2(request):
+    return render(request,'shop/productframe2.html')
+
+def productframe3(request):
+    return render(request,'shop/productframe3.html')
+
+def productframe4(request):
+    return render(request,'shop/productframe4.html')
+
+def shoplogin(request):
+    return render(request,'shop/shoplogin.html')
